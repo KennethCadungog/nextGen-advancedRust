c37a7b8628ec85489db689c1f67d77226a529097
KennethCadungog-Trinity University <112190731+KennethCadungog@users.noreply.github.com>
2024-04-16 10:41:51 -0500

    Uncomment line 3

--- python/squares_sum.py
+++ python/squares_sum.py
@@ -4,7 +4,7 @@ import pro_rust
 
 numbers:list[int] = [1, 2, 3, 4, 5]
 result:int = pro_rust.sum_of_squares_python(numbers)
-#print("Sum of squares: ", result)
+print("Sum of squares: ", result)
 
 
 def sum_of_squares(numbers:list[int]) -> int:
